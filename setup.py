6a0206c4dfba558a783a510a0a6d377b888441c5
Thomas Kluyver <thomas@kluyver.me.uk>
2018-06-07 14:25:51 +0200

    Add Travis CI configuration

--- setup.py
+++ setup.py
@@ -37,6 +37,12 @@ setup(name="karabo_bridge",
           'numpy',
           'pyzmq>=17.0.0',
       ],
+      extras_require={
+          'test': [
+              'pytest',
+              'pytest-cov',
+          ]
+      },
       packages=find_packages(),
       entry_points={
           'console_scripts': [
