edb4651cf90b9886f84365fd5579d8a57857afb9
Thomas Michelat <thomas.michelat@xfel.eu>
2018-06-09 01:29:43 +0200

    handle bytes

--- karabo_bridge/cli/glimpse.py
+++ karabo_bridge/cli/glimpse.py
@@ -52,11 +52,8 @@ def walk_dict_to_hdf5(dic, h5):
             h5.create_dataset(key, data=value, dtype=value.dtype)
         elif isinstance(value, (int, float)):
             h5.create_dataset(key, data=value, dtype=type(value))
-        elif isinstance(value, str):
-            dt = h5py.special_dtype(vlen=str)
-            h5.create_dataset(key, data=str(value), dtype=dt)
-        elif isinstance(value, bytes):
-            dt = h5py.special_dtype(vlen=bytes)
+        elif isinstance(value, (str, bytes):
+            dt = h5py.special_dtype(vlen=type(value))
             h5.create_dataset(key, data=value, dtype=dt)
         else:
             print('not supported', type(value))
