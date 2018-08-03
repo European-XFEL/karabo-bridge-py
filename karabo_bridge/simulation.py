c2983a27ec7ba9e3db4c9c79b0a3241b9946f000
Thomas Kluyver <thomas.kluyver@xfel.eu>
2018-08-03 13:54:20 +0200

    Use zero-copy send in simulator

--- karabo_bridge/simulation.py
+++ karabo_bridge/simulation.py
@@ -310,7 +310,7 @@ def start_gen(port, ser='msgpack', version='2.2', detector='AGIPD',
             if msg == b'next':
                 train = next(generator)
                 msg = containize(train, ser, serialize, version)
-                socket.send_multipart(msg)
+                socket.send_multipart(msg, copy=False)
                 if debug:
                     print('Server : emitted train:',
                           train[1][list(train[1].keys())[0]]['timestamp.tid'])
@@ -365,7 +365,7 @@ class ServeInThread(Thread):
                     train = next(self.generator)
                     msg = containize(train, self.serialization_fmt,
                                      self.serialize, self.protocol_version)
-                    self.server_socket.send_multipart(msg)
+                    self.server_socket.send_multipart(msg, copy=False)
                 else:
                     print('Unrecognised request:', msg)
             elif self.stopper_r in events:
